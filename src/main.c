cf49cc91fe71fb3d9db26cb0b625a16064f5beee
Julian Achatzi <julian.achatzi@fh-koeln.de>
2015-07-24 00:13:50 +0200

    added a simple getVoltage function

--- src/main.c
+++ src/main.c
@@ -3,6 +3,12 @@
 #include "ADC.h"
 #include "Serial.h"
 #include "SPI.h"
+int VREF = 5;
+int BITS = 10;
+
+float getVoltage(uint16_t adcValue) {
+	return VREF*((float)adcValue)/(2^(BITS));
+}
 
 int main(void) {
 	ADC_initializeHardware();
@@ -10,9 +16,14 @@ int main(void) {
 	SPI_initializeHardware();
 
 	for (;;) {
-		Serial_print("ADC: ");
-		Serial_printIntegerAndReturn(ADC_readValue(0), 10);
+		Serial_print("Voltage: ");
+		Serial_printInteger(getVoltage(ADC_readValue(0)), 10);
+		Serial_print("V\n");
 
 		_delay_ms(250);
 	}
 }
+
+
+
+
